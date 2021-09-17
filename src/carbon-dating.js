e1be16f41bbf24e3fd258ceafd6d16082f2e2212
AndreyPesh <coursestud@mail.ru>
2021-09-17 19:09:59 +0300

    feat: decide carbon-dating and count-cats

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -17,7 +17,10 @@ const HALF_LIFE_PERIOD = 5730;
  * dateSample('WOOT!') => false
  *
  */
-export default function dateSample(/* sampleActivity */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function dateSample(sampleActivity) {
+  if (!sampleActivity || !Number(sampleActivity) || typeof sampleActivity !== 'string') {
+    return false
+  }
+  const time = HALF_LIFE_PERIOD * Math.log2((MODERN_ACTIVITY / +sampleActivity))
+  return time >= 0  ? Math.ceil(time) : false
 }
--- src/count-cats.js
+++ src/count-cats.js
@@ -14,7 +14,17 @@ import { NotImplementedError } from '../extensions/index.js';
  * ]) => 3`
  *
  */
-export default function countCats(/* matrix */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function countCats(matrix) {
+  let count = 0
+  let index = 0
+  const length = matrix.length
+  while(index < length) {
+    matrix[index].forEach(cat => {
+        if (cat == '^^') {
+          count++
+        }
+      })
+    index++
+  }
+  return count
 }
