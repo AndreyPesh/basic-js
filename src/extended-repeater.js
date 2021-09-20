0d50464219158b044b1a28d61623a7658b1e1d94
AndreyPesh <coursestud@mail.ru>
2021-09-20 13:20:47 +0300

    feat: decide extended repeater

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -15,7 +15,57 @@ import { NotImplementedError } from '../extensions/index.js';
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
  */
-export default function repeater(/* str, options */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function repeater(str, options) {
+  let initialString = str 
+  if (typeof initialString !== 'string'){
+    initialString = String(initialString)
+  }
+  let arrayRepeater = new Array(initialString)
+  let addition = ''
+  let additionSeparator = '|'
+  let additionRepeatTimes = 1
+  let separator = '+'
+  let repeatTimes = 1
+  let resultAddition = ''
+  let resultString = ''
+  for (const option in options) {
+    if (Object.prototype.hasOwnProperty.call(options, option)) {
+      if (option === 'addition') {
+        addition = options[option]
+        if (typeof addition !== 'string') {
+          addition = String(addition)
+        }
+        
+      } else if (option === 'additionSeparator') {
+        additionSeparator = options[option]
+      } else if (option === 'additionRepeatTimes') {
+        additionRepeatTimes = options[option]
+      } else if (option === 'separator') {
+        separator = options[option]
+      } else if (option === 'repeatTimes') {
+        repeatTimes = options[option]
+      }
+    }
+  }
+  arrayRepeater = arrayRepeater.map(element => {
+    for (let i = 0; i < additionRepeatTimes; i++) {
+      if (i === additionRepeatTimes - 1) {
+        resultAddition +=addition
+        continue
+      }
+      resultAddition += addition + additionSeparator
+    }
+    return element + resultAddition
+  })
+  arrayRepeater = arrayRepeater.map(element => {
+    for (let i = 0; i < repeatTimes; i++) {
+      if (i === repeatTimes - 1) {
+        resultString += element
+        continue
+      }
+      resultString += element + separator
+    }
+    return resultString
+  })
+  return arrayRepeater.join('')
 }
