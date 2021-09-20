91b48a35af675c7424bd6e8418b759bddf30fa6a
AndreyPesh <coursestud@mail.ru>
2021-09-20 23:04:23 +0300

    feat: decide common charater count

--- src/st-common-character-count.js
+++ src/st-common-character-count.js
@@ -11,7 +11,15 @@ import { NotImplementedError } from '../extensions/index.js';
  * For s1 = "aabcc" and s2 = "adcaa", the output should be 3
  * Strings have 3 common characters - 2 "a"s and 1 "c".
  */
-export default function getCommonCharacterCount(/* s1, s2 */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function getCommonCharacterCount(s1, s2) {
+  const stringArray = s1.split('')
+  const string2 = s2.split('')
+  return stringArray.reduce((acc, char) => {
+    const indexChar = string2.indexOf(char)
+    if (indexChar !== -1) {
+      string2.splice(indexChar, 1)
+      acc++
+    }
+    return acc
+  }, 0)
 }
