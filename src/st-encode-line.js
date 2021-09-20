5790685d842f1715601bf7d969829bc651d09ce4
AndreyPesh <coursestud@mail.ru>
2021-09-20 23:59:49 +0300

    feat: decide encode line

--- src/st-encode-line.js
+++ src/st-encode-line.js
@@ -10,7 +10,18 @@ import { NotImplementedError } from '../extensions/index.js';
  * For aabbbc should return 2a3bc
  *
  */
-export default function encodeLine(/* str */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function encodeLine(str) {
+  const charsArray = str.split('')
+  let template = ''
+  let counter = 1
+  for (let i = 0; i < charsArray.length; i++) {
+    if (charsArray[i] === charsArray[i + 1]) {
+      counter++
+    } else {
+      template += counter > 1 ? counter + charsArray[i] : charsArray[i]
+      counter = 1
+    }
+  }
+  return template
+
 }
